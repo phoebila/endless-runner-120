4e3f5deeaf936bca53fc1a1b8a5b033423e12ec5
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-01-30 12:37:39 -0800

    adding score display to ending

--- src/scenes/Ending.js
+++ src/scenes/Ending.js
@@ -6,6 +6,21 @@ class Ending extends Phaser.Scene {
     create(score){
         //display game over screen w/ score
 
+        // display score number
+        let scoreConfig = {
+            fontFamily: 'quicksand',
+            fontSize: '16px',
+            backgroundColor: '#b3325f',
+            color: '#000',
+            align: 'right',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.add.text(game.config.width/2, game.config.height/2 + 64, `Score: ${score}`, scoreConfig).setOrigin(0.5)
+
         //set up cursor keys
         keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
     }
