53576ddba181c779b8df9267032d82397120e22d
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-01-30 12:34:58 -0800

    adding yumi right and jump movement

--- src/prefabs/yumi.js
+++ src/prefabs/yumi.js
@@ -1,19 +1,25 @@
 class Yumi extends Phaser.GameObjects.Sprite {
-    moveable = true
-    setCollideWorldBounds = true
-    enableBody = true
-
     constructor(){
         super(scene, x, y, texture, frame)
         scene.add.existing(this)
+
+        this.moveable = true
+        this.setCollideWorldBounds = true
+        this.enableBody = true
     }
 
-    update(){ //fix?
+    update(){
+        // right movement
         if (cursors.right.isDown && this.moveable){
-            if (this.x > game.config.width/3 +20){
+            if (this.x > this.x >= borderUISize + this.width){
                 // move yumi right
-                this.x -= game.settings.scrollSpeed/2
+                this.x += game.settings.scrollSpeed/2
             }
+        }
+
+        // jump movement
+        if (cursors.up.isDown && this.y >= borderUISize * 3 + borderPadding){
+            this.y -= 20
         }
     }
 }
